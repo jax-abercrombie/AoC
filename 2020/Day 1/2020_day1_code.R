82decd272a84170e671d5ffc7413bdb11c50a110
Abercrombie, Jaxon {MDBN~South San Francisco} <abercrombie.jaxon@gene.com>
2023-03-31 01:36:37 +0200

    commit

--- 2020/Day 1/2020_day1_code.R
+++ 2020/Day 1/2020_day1_code.R
@@ -1,7 +1,7 @@
 # Link to task: https://adventofcode.com/2020/day/1
 
-# Import list
-expense_list_df <- read.delim("~/AoC/2020/Day 1/puzzle_input.txt")
+# Import input
+expense_list_df <- read.delim("~/AoC/2020/Day 1/2020_day1_input.txt")
 expense_list <- as.vector(expense_list_df$X1348)
 
 # Make all combinations from list of expenses
@@ -9,14 +9,14 @@ expense_combos <- combn(expense_list, 2, simplify = TRUE)
 
 # Make loop to iterate over all combinations, sum them to see if they equal 2020, and mutliply them if so
 answer_vec <- c()
-for (combo in 1:length((expense_combos)/2)-2){
-  combo_sum <- sum(expense_combos[combo+2:combo+2])
+for (combo in 1:ncol(expense_combos)){
+  combo_sum <- sum(expense_combos[,combo])
   if (combo_sum == 2020){
-    answer_vec <- c(answer_vec, expense_combos[combo], expense_combos[combo+1])
-    product <- (expense_combos[combo])*(expense_combos[combo+1])
-    answer_vec <- c(answer_vec, product)
+    answer_vec <- c(answer_vec, expense_combos[,combo])
+    prod <- (expense_combos[2,combo])*(expense_combos[1,combo])
+    answer_vec <- c(answer_vec, prod)
+    }
   }
-}
 
 # Answer
 print(answer_vec)
---
+++ 2022/Day 1/2022_day1_code.R
@@ -0,0 +1,26 @@
+# Link to task: https://adventofcode.com/2020/day/1
+
+library(dplyr)
+
+# Import input
+carry_list_df <- read.table("~/AoC/2022/Day 1/2022_day1_input.txt", blank.lines.skip = FALSE)
+# carry_list <- as.vector(carry_list_df$X7569)
+
+
+# Initialize variables
+calorie_sum <- 0
+calorie_sum_list <- c()
+
+# Create pipe that groups based on NAs and 
+carry_list_df <-
+  carry_list_df %>% 
+  mutate(elf = as.character(cumsum(is.na(V1)) + !is.na(V1))) %>%
+  filter(!is.na(V1))
+
+
+# Answer
+carry_list_df %>%
+  group_by(elf) %>%
+  summarise(sum = sum(V1)) %>%
+  arrange(desc(sum)) %>%
+  slice(1)
