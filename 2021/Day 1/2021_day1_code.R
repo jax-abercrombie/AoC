3bcb60a2eb77ca46389549c777ecd651e0c22a9f
Abercrombie, Jaxon {MDBN~South San Francisco} <abercrombie.jaxon@gene.com>
2023-04-05 20:05:51 +0200

    2021 day 1 done

--- 2021/Day 1/2021_day1_code.R
+++ 2021/Day 1/2021_day1_code.R
@@ -1,1 +1,24 @@
 # Link to task: https://adventofcode.com/2021/day/1
+
+# Import input
+sonar <- read.table("~/AoC/2021/Day 1/2021_day1_input.txt", blank.lines.skip = FALSE)
+
+# First attempt (easy for loop and counter)
+count <- 0
+for (m in 1:(nrow(sonar)-1)){
+  m1 <- sonar$V1[m]
+  m2 <- sonar$V1[m+1]
+  if (m2 > m1){
+    count <- count + 1
+  }
+}
+print(count)
+
+
+# Second attempt
+
+
+sonar2 <- 
+  sonar %>%
+  
+
