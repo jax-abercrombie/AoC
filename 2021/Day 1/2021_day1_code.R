282da9a02fcc6e9f6340968fc4beacc1364d8d2f
Abercrombie, Jaxon {MDBN~South San Francisco} <abercrombie.jaxon@gene.com>
2023-04-05 20:06:31 +0200

    commit

--- 2021/Day 1/2021_day1_code.R
+++ 2021/Day 1/2021_day1_code.R
@@ -4,6 +4,7 @@
 sonar <- read.table("~/AoC/2021/Day 1/2021_day1_input.txt", blank.lines.skip = FALSE)
 
 # First attempt (easy for loop and counter)
+# Part 1
 count <- 0
 for (m in 1:(nrow(sonar)-1)){
   m1 <- sonar$V1[m]
@@ -15,10 +16,38 @@ for (m in 1:(nrow(sonar)-1)){
 print(count)
 
 
-# Second attempt
 
+
+# Part 2
+count2 <- 0
+
+for (m in 1:(nrow(sonar)-3)){
+  m1 <- sonar$V1[m]
+  m2 <- sonar$V1[m+1]
+  m3 <- sonar$V1[m+2]
+  m4 <- sonar$V1[m+3]
+  sum1 <- m1 + m2 + m3
+  sum2 <- m2 + m3 + m4
+  if (sum1 < sum2){
+    count2 <- count2 + 1
+  }
+}
+print(count2)
+
+
+
+
+# Second attempt
+sonar$index <- 1:nrow(sonar)
 
 sonar2 <- 
-  sonar %>%
-  
+  sonar %>% 
+  arrange(V1)
 
+sonar2$index_ascend <- 1:nrow(sonar)
+
+sonar2 <-
+  sonar2 %>%
+  mutate(diff = index - index_ascend)
+
+2000-nrow(sonar2)
